3e1719e9522ad3c87d7035e63ec4c8159fb6e97f
Patrick von Platen <patrick.v.platen@gmail.com>
2020-06-16 19:14:19 +0200

    add info for fp16

--- src/transformers/benchmark/benchmark_tf.py
+++ src/transformers/benchmark/benchmark_tf.py
@@ -141,6 +141,7 @@ class TensorflowBenchmark(Benchmark):
         config = self.config_dict[model_name]
 
         if self.args.fp16:
+            logger.info("Running model with mixed precision using keras experimental policy")
             assert self.is_gpu, "Mixed Precision using keras experimental policy is only allowed for GPU."
             policy = tf.keras.mixed_precision.experimental.Policy("mixed_float16")
             tf.keras.mixed_precision.experimental.set_policy(policy)
